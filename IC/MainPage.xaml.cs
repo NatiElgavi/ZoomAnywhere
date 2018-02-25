2f3eaa326fec2b0007ab9337b31a115465628bdb
Nati Elgavi <nati.elgavi@gmail.com>
2018-02-26 01:08:50 +0200

    Adding basic version with translation only.

--- IC/MainPage.xaml.cs
+++ IC/MainPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Media.Capture;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -22,9 +23,85 @@ namespace IC
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private MediaCapturePreviewer _previewer = null;
+        private TranslateTransform dragTranslation;
+        private TranslateTransform dragTranslationCapture;
+
         public MainPage()
         {
             this.InitializeComponent();
+            _previewer = new MediaCapturePreviewer(captureElement, Dispatcher);
+
+            touchRectangle.ManipulationDelta += touchRectangle_ManipulationDelta;
+            dragTranslation = new TranslateTransform();
+            touchRectangle.RenderTransform = this.dragTranslation;
+
+            captureElement.ManipulationDelta += captureElement_ManipulationDelta;
+            dragTranslationCapture = new TranslateTransform();
+            captureElement.RenderTransform = this.dragTranslationCapture;
+        }
+
+        void touchRectangle_ManipulationDelta(object sender, ManipulationDeltaRoutedEventArgs e)
+        {
+            // Move the rectangle.
+            dragTranslation.X += e.Delta.Translation.X;
+            dragTranslation.Y += e.Delta.Translation.Y;
+        }
+
+        private void captureElement_ManipulationDelta(object sender, ManipulationDeltaRoutedEventArgs e)
+        {
+            // Move the rectangle.
+            dragTranslationCapture.X += e.Delta.Translation.X;
+            dragTranslationCapture.Y += e.Delta.Translation.Y;
+        }
+
+        protected override async void OnNavigatedTo(NavigationEventArgs e)
+        {
+            await _previewer.InitializeCameraAsync();
+
+            if (_previewer.IsPreviewing)
+            {
+                PopulateResolutionsComboBox();
+                var desiredItem = resolutions.Items[resolutions.Items.Count - 1] as ComboBoxItem;
+                var encodingProperties = (desiredItem.Tag as StreamResolution).EncodingProperties;
+                await _previewer.SetMediaStreamPropertiesAsync(MediaStreamType.VideoPreview, encodingProperties);
+            }
+        }
+
+        protected override async void OnNavigatingFrom(NavigatingCancelEventArgs e)
+        {
+            await _previewer.CleanupCameraAsync();
+        }
+
+        private void PopulateResolutionsComboBox()
+        {
+            // Query all properties of the device
+            IEnumerable<StreamResolution> allProperties = _previewer.MediaCapture.VideoDeviceController.GetAvailableMediaStreamProperties(MediaStreamType.VideoPreview).Select(x => new StreamResolution(x));
+
+            // Order them by resolution then frame rate
+            allProperties = allProperties.OrderByDescending(x => x.Height * x.Width).ThenByDescending(x => x.FrameRate);
+
+            // Populate the combo box with the entries
+            foreach (var property in allProperties)
+            {
+                if ((property.Width > 4000) && (property.FrameRate > 25))
+                {
+                    ComboBoxItem comboBoxItem = new ComboBoxItem();
+                    comboBoxItem.Content = property.GetFriendlyName();
+                    comboBoxItem.Tag = property;
+                    resolutions.Items.Add(comboBoxItem);
+                }
+            }
+        }
+
+        private async void resolutions_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (_previewer.IsPreviewing)
+            {
+                var selectedItem = (sender as ComboBox).SelectedItem as ComboBoxItem;
+                var encodingProperties = (selectedItem.Tag as StreamResolution).EncodingProperties;
+                await _previewer.SetMediaStreamPropertiesAsync(MediaStreamType.VideoPreview, encodingProperties);
+            }
         }
     }
 }
---
+++ IC/MediaCapturePreviewer.cs
@@ -0,0 +1,97 @@
+﻿using System.Threading.Tasks;
+using Windows.Media.Capture;
+using Windows.Media.MediaProperties;
+using Windows.UI.Core;
+using Windows.UI.Xaml.Controls;
+using System;
+
+namespace IC
+{
+    internal class MediaCapturePreviewer
+    {
+        CoreDispatcher _dispatcher;
+        CaptureElement _previewControl;
+
+        public MediaCapturePreviewer(CaptureElement previewControl, CoreDispatcher dispatcher)
+        {
+            _previewControl = previewControl;
+            _dispatcher = dispatcher;
+        }
+
+        public bool IsPreviewing { get; private set; }
+        public bool IsRecording { get; set; }
+        public MediaCapture MediaCapture { get; private set; }
+
+        /// <summary>
+        /// Sets encoding properties on a camera stream. Ensures CaptureElement and preview stream are stopped before setting properties.
+        /// </summary>
+        public async Task SetMediaStreamPropertiesAsync(MediaStreamType streamType, IMediaEncodingProperties encodingProperties)
+        {
+            // Stop preview and unlink the CaptureElement from the MediaCapture object
+            await MediaCapture.StopPreviewAsync();
+            _previewControl.Source = null;
+
+            // Apply desired stream properties
+            await MediaCapture.VideoDeviceController.SetMediaStreamPropertiesAsync(MediaStreamType.VideoPreview, encodingProperties);
+
+            // Recreate the CaptureElement pipeline and restart the preview
+            _previewControl.Source = MediaCapture;
+            await MediaCapture.StartPreviewAsync();
+        }
+
+        /// <summary>
+        /// Initializes the MediaCapture, starts preview.
+        /// </summary>
+        public async Task InitializeCameraAsync()
+        {
+            MediaCapture = new MediaCapture();
+            MediaCapture.Failed += MediaCapture_Failed;
+
+            try
+            {
+                await MediaCapture.InitializeAsync();
+                _previewControl.Source = MediaCapture;
+                await MediaCapture.StartPreviewAsync();
+                IsPreviewing = true;
+            }
+            catch (UnauthorizedAccessException)
+            {
+                // This can happen if access to the camera has been revoked.
+                await CleanupCameraAsync();
+            }
+        }
+
+        public async Task CleanupCameraAsync()
+        {
+            if (IsRecording)
+            {
+                await MediaCapture.StopRecordAsync();
+                IsRecording = false;
+            }
+
+            if (IsPreviewing)
+            {
+                await MediaCapture.StopPreviewAsync();
+                IsPreviewing = false;
+            }
+
+            _previewControl.Source = null;
+
+            if (MediaCapture != null)
+            {
+                MediaCapture.Dispose();
+                MediaCapture = null;
+            }
+        }
+
+        private void MediaCapture_Failed(MediaCapture sender, MediaCaptureFailedEventArgs e)
+        {
+            var task = _dispatcher.RunAsync(CoreDispatcherPriority.Normal, async () =>
+            {
+                IsRecording = false;
+                IsPreviewing = false;
+                await CleanupCameraAsync();
+            });
+        }
+    }
+}
